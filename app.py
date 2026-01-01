a8e9608e1fedc3dad7d366007ae5db78f6317ebc
Srividya Kollepu <srividyakollepu325@gmail.com>
2026-01-01 19:45:39 +0530

    Update app.py
    added echo function

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class EchoInput(BaseModel):
+    message: str
 
 @app.get("/")
 def home():
@@ -9,3 +13,6 @@ def home():
 @app.get("/health")
 def health():
     return {"status": "healthy"}
+@app.post("/echo")
+def echo(data:EchoInput):
+    return {"you sent: ",data.message}
